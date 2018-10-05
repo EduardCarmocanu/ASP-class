99fe9ee340d57cf4b3c4dc94d6c6e1e4f1065675
Ion Toporan <ioto@eal.dk>
2018-10-05 10:47:45 +0200

    Code from class

---
+++ UclLib/Data/GetData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UclLib.Objects;
+
+namespace UclLib.Data
+{
+    public static class GetData
+    {
+        public static List<Student> GetStudents()
+        {
+            List<Student> studentsList = new List<Student>();
+            for(int i = 1; i <= 45; i++)
+            {
+                Student student = new Student()
+                {
+                    Address = new Address(),
+                    CPR = 12345678 + i,
+                    Email = string.Format("student{0}@edu.ucl.dk", i),
+                    IsErasmusStudent = false,
+                    Name = "Student Doe" + i,
+                    Phone = 87964512 + i
+                };
+                studentsList.Add(student);
+            }
+            return studentsList;
+        }
+
+        public static List<Teacher> GetTeachers()
+        {
+            List<Teacher> teachersList = new List<Teacher>();
+            for (int i = 1; i <= 45; i++)
+            {
+                Teacher teacher = new Teacher()
+                {
+                    Address = new Address(),
+                    CPR = 12345678 + i,
+                    Email = string.Format("teacher{0}@ucl.dk", i),
+                    Name = "Teacher Doe" + i,
+                    Phone = 87964512 + i
+                };
+                teachersList.Add(teacher);
+            }
+            return teachersList;
+        }
+
+        // TODO: Create a combined list of students AND teachers
+        public static List<Person> GetAllUclData()
+        {
+            List<Person> personsList = new List<Person>();
+            personsList.AddRange(GetStudents());
+            personsList.AddRange(GetTeachers());
+
+            return personsList;
+        }
+    }
+}
---
+++ UclLib/Objects/Address.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UclLib.Objects
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public int Zip { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public Address(string _street, int _zip, string _city, string _country)
+        {
+            Street = _street;
+            Zip = _zip;
+            City = _city;
+            Country = _country;
+        }
+        public Address() { }
+    }
+
+    public class AddressTest
+    {
+        public void Test()
+        {
+            Address myAddress = new Address();
+            myAddress.Country = "Denmark";
+            myAddress.Zip = 1234;
+
+            Address a1 = new Address("Street", 123, "Odense", "DK");
+        }
+    }
+}
