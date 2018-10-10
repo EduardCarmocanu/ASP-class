08dc02d27c1347d9fc140d95ac68a19088975477
Carmocanu Eduard <carmocanueduard@gmail.com>
2018-10-10 09:40:08 +0200

    Retrieved data from JSON and created simple interface with modal

--- UclLib/Data/GetData.cs
+++ UclLib/Data/GetData.cs
@@ -1,50 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Web;
+using System.IO;
+using Newtonsoft.Json;
 using UclLib.Objects;
+
 
 namespace UclLib.Data
 {
-    public static class GetData
+	public static class GetData
     {
+		public static string ReadFile(string path)
+		{
+			return File.ReadAllText(HttpContext.Current.Server.MapPath(path));
+		}
+
         public static List<Student> GetStudents()
         {
-            List<Student> studentsList = new List<Student>();
-            for(int i = 1; i <= 45; i++)
-            {
-                Student student = new Student()
-                {
-                    Address = new Address(),
-                    CPR = 12345678 + i,
-                    Email = string.Format("student{0}@edu.ucl.dk", i),
-                    IsErasmusStudent = false,
-                    Name = "Student Doe" + i,
-                    Phone = 87964512 + i
-                };
-                studentsList.Add(student);
-            }
-            return studentsList;
-        }
+            return JsonConvert.DeserializeObject<List<Student>>(ReadFile("~/db/StudentsData.json"));
+		}
 
         public static List<Teacher> GetTeachers()
         {
-            List<Teacher> teachersList = new List<Teacher>();
-            for (int i = 1; i <= 45; i++)
-            {
-                Teacher teacher = new Teacher()
-                {
-                    Address = new Address(),
-                    CPR = 12345678 + i,
-                    Email = string.Format("teacher{0}@ucl.dk", i),
-                    Name = "Teacher Doe" + i,
-                    Phone = 87964512 + i
-                };
-                teachersList.Add(teacher);
-            }
-            return teachersList;
-        }
+			return JsonConvert.DeserializeObject<List<Teacher>>(ReadFile("~/db/TeachersData.json"));
+		}
 
         // TODO: Create a combined list of students AND teachers
         public static List<Person> GetAllUclData()
